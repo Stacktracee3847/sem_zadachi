767ca17595be4a2b77871bee217ad5ec6318577a
Stacktracee3847 <timofejc83@gmail.com>
2025-12-27 12:58:05 +0300

    1

--- ex11sem4.cpp
+++ ex11sem4.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
+// 1. Класс Engine
 class Engine {
 private:
-    double thrust;    // Н
-    double fuelFlow;  // кг/с
-    double fuel;      // кг
-public:
-    Engine(double t, double ff, double f) : thrust(t), fuelFlow(ff), fuel(f) {}
+    double thrust;
+    double fuelFlow;
+    double fuel;
 
-    double getThrust() const { return thrust; }
-    bool hasFuel() const { return fuel > 0; }
+public:
+    Engine(double thr, double flow, double f)
+        : thrust(thr), fuelFlow(flow), fuel(f) {
+    }
+
+    double getThrust() const {
+        return thrust;
+    }
+
+    bool hasFuel() const {
+        return fuel > 0;
+    }
 
     void burn(double dt) {
-        if (fuel <= 0) return;
-        double consumed = fuelFlow * dt;
-        if (consumed > fuel) consumed = fuel;
-        fuel -= consumed;
+        if (hasFuel()) {
+            double consumed = fuelFlow * dt;
+            if (consumed > fuel) consumed = fuel;
+            fuel -= consumed;
+        }
+    }
+
+    double getFuel() const {
+        return fuel;
     }
 };
 
+// 2. Класс Navigation
 class Navigation {
 private:
-    double altitude;      // м
-    double velocity;      // м/с
-    double acceleration;  // м/с²
-    double mass;          // кг
+    double altitude;
+    double velocity;
+    double acceleration;
+    double mass;
+
 public:
-    Navigation(double h, double v, double m) : altitude(h), velocity(v), mass(m), acceleration(0) {}
+    Navigation(double alt, double vel, double m)
+        : altitude(alt), velocity(vel), mass(m), acceleration(0) {
+    }
 
     void update(double thrust, double dt) {
         acceleration = thrust / mass - 9.81;
@@ -37,20 +56,27 @@ public:
     }
 
     void printStatus(double time) const {
-        std::cout << std::fixed << std::setprecision(2);
-        std::cout << "t=" << time << "c | h=" << altitude << "м | v=" 
-                  << velocity << "м/с | a=" << acceleration << "м/с²" << std::endl;
+        cout << fixed << setprecision(2);
+        cout << "t=" << time << "с | h=" << altitude
+            << "м | v=" << velocity << "м/с | a=" << acceleration << "м/с²" << endl;
+    }
+
+    double getAltitude() const {
+        return altitude;
     }
 };
 
+// 3. Класс AutonomousFlightSystem
 class AutonomousFlightSystem {
 private:
     Engine engine;
     Navigation nav;
     double time;
+
 public:
-    AutonomousFlightSystem(const Engine& e, const Navigation& n) 
-        : engine(e), nav(n), time(0) {}
+    AutonomousFlightSystem(const Engine& e, const Navigation& n)
+        : engine(e), nav(n), time(0) {
+    }
 
     void simulate(double dt, double totalTime) {
         while (time < totalTime && engine.hasFuel()) {
@@ -59,16 +85,22 @@ public:
             nav.printStatus(time);
             time += dt;
         }
-        std::cout << "--- Полёт завершён ---" << std::endl;
+    }
+
+    void printSummary() const {
+        cout << "--- Полет завершен ---" << endl;
+        cout << "Оставшееся топливо: " << engine.getFuel() << " кг" << endl;
+        cout << "Итоговая высота: " << nav.getAltitude() << " м" << endl;
     }
 };
 
 int main() {
-    Engine eng(15000, 5, 50);      // тяга 15000 Н, расход 5 кг/с, топливо 50 кг
-    Navigation nav(0, 0, 1000);    // высота 0, скорость 0, масса 1000 кг
-    AutonomousFlightSystem afs(eng, nav);
-    
-    afs.simulate(1.0, 20.0);
-    
+    Engine engine(12000, 2.5, 50);   // тяга, расход, топливо
+    Navigation nav(0, 0, 1000);      // высота, скорость, масса
+    AutonomousFlightSystem system(engine, nav);
+
+    system.simulate(1.0, 10.0);
+    system.printSummary();
+
     return 0;
 }
